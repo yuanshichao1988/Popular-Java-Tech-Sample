d466200dc6399d00f051e33ec6b97c7a087ac862
yuanshichao <wsm12345>
2018-05-07 09:42:33 +0800

    add java folder

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Popular-Java-Tech-Sample
 个人收集的主流Java技术及示例
+
+### 一、第三方工具类
+
+#### 1、Guava
---
+++ src/main/java/com/ysc/App.java
@@ -0,0 +1,13 @@
+package com.ysc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
